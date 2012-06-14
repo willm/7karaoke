285641e271d1ad5612cb84fa49e643233b2d6fac
Will Munn <willmunn@hotmail.com>
2012-06-14 21:31:39 +0200

    I'm  the king of the clojure

--- mmeasy.js
+++ mmeasy.js
@@ -3,20 +3,19 @@ var mXm  = require("musixmatch");
 
 mXm.Config.API_KEY = "b386a62dc65c5a811e861981a22bda94";
 
-MusixMatchParser = function (){};
-
-MusixMatchParser.prototype.parseResponse = function (musixMatchResponse){
-	var lyrics = musixMatchResponse.attributes.subtitle_body;
-	var splitTimeStampsAndLyrics = function (){
-		var lyricsAndTimeStamps = lyrics.split(/\[(.*?)\]/).slice(1);
-		var niceObject = {};
-		for(var i =0; i<lyricsAndTimeStamps.length; i+=2){
-			niceObject[lyricsAndTimeStamps[i]] = lyricsAndTimeStamps[i+1];
+MusixMatchParser = function (res){ 
+	this.res = res;
+	this.parseResponse = (function(that){
+		return function (musixMatchResponse){
+			var lyrics = musixMatchResponse.attributes.subtitle_body;
+			var lyricsAndTimeStamps = lyrics.split(/\[(.*?)\]/).slice(1);
+			var niceObject = {};
+			for(var i =0; i<lyricsAndTimeStamps.length; i+=2){
+				niceObject[lyricsAndTimeStamps[i]] = lyricsAndTimeStamps[i+1];
+			}
+			console.log(that.res.send(niceObject));
 		}
-		console.log(niceObject);
-		return niceObject;
-	}
-	splitTimeStampsAndLyrics(lyrics);
+	}(this));
 }
 
 MusixMatchParser.prototype.onError = function(response) {
@@ -26,7 +25,10 @@ MusixMatchParser.prototype.onError = function(response) {
 
 MusixMatchParser.prototype.getTrackLyrics = function(trackId){
 	var that = this;
-	mXm.API.getSubtitle(14201829, that.parseResponse, that.onError);
+	return mXm.API.getSubtitle(14201829, that.parseResponse, that.onError);
 };
 
-new MusixMatchParser().getTrackLyrics(14201829);
+//new MusixMatchParser().getTrackLyrics(14201829);
+exports.create = function(res){
+	return new MusixMatchParser(res);
+}
--- musixmatch.js
+++ musixmatch.js
@@ -1,18 +1,60 @@
-var http = require('http');
+var http = require('http'),
+	util = require('util');
+/*
+function makeRequest(){
+	var options = {
+	  host: 'api.musixmatch.com',
+	  port: 80,
+	  path: '/ws/1.1/track.subtitle.get?track_mbid=e2c9d3b3-8d55-4979-b54c-ccb3cd6dde74&apikey=b386a62dc65c5a811e861981a22bda94&format=xml',
+	  method: 'GET'
+	};
+	http.request(options, function(resp){
+	resp.setEncoding('utf8');
+	resp.on('data', function(chunk){
+    	console.log(chunk);
+  	});
+		//processData(resp);
+		this.response = resp;
+	}).on("error", function(e){
+	  console.log("Got error: " + e.message);
+	});
+}
+
+*/
+
+
+function processData(musixmatchResponse){
+	var lyrics = musixmatchResponse.body.subtitle.subtitle_body;
+
+	var splitTimeStampsAndLyrics = function (){
+		var lyricsAndTimeStamps = lyrics.split(/\[(.*?)\]/).slice(1);
+		var niceObject = {};
+		for(var i =0; i<lyricsAndTimeStamps.length; i+=2){
+			niceObject[lyricsAndTimeStamps[i]] = lyricsAndTimeStamps[i+1];
+		}
+		console.log(niceObject);
+		return niceObject;
+	}
+}
 
 var options = {
   host: 'api.musixmatch.com',
   port: 80,
-  path: '/ws/1.1/track.subtitle.get?track_mbid=e2c9d3b3-8d55-4979-b54c-ccb3cd6dde74&apikey=b386a62dc65c5a811e861981a22bda94&format=xml'
+  path: '/ws/1.1/track.subtitle.get?track_mbid=e2c9d3b3-8d55-4979-b54c-ccb3cd6dde74&apikey=b386a62dc65c5a811e861981a22bda94&format',
+  method: 'GET',
+  headers: {'content-type':'application/json'}
 };
 
-http.get(options, function(resp){
-  //resp.on('data', function(chunk){
-    console.log(resp);
-    //do something with chunk
-  //});
-}).on("error", function(e){
-  console.log("Got error: " + e.message);
+var req = http.request(options, function(res) {
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log(util.inspect(chunk))
+    //console.log(JSON.parse(chunk));
+  });
 });
 
-//http://api.musixmatch.com/ws/1.1/track.subtitle.get?track_mbid=e2c9d3b3-8d55-4979-b54c-ccb3cd6dde74&apikey=b386a62dc65c5a811e861981a22bda94&format=xml
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+req.end();
